f28b9cf2f896c728a4a69f8cbb0f0a73d1868fc5
Simone Taeggi <startae14@gmail.com>
2019-05-19 17:50:17 +0200

    created the Wallet Component to pass the test connected to the Store

--- src/components/Wallet.js
+++ src/components/Wallet.js
@@ -1,15 +1,23 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import balance from '../reducers/balance';
 
-class Wallet extends Component {
+export class Wallet extends Component {
     render() {
         return (
             <div>
                 <h3>
-                    Wallet Balance
+                    Wallet Balance: {this.props.balance}
                 </h3>
             </div>
         );
     }
 }
 
-export default Wallet;
+const mapStateToProps = state => {
+    return {
+        balance: state
+    };
+}
+
+export default connect(mapStateToProps, null)(Wallet);
--- src/components/Wallet.test.js
+++ src/components/Wallet.test.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Adapter from 'enzyme-adapter-react-16';
 import { shallow, configure } from 'enzyme';
 import toJson from 'enzyme-to-json';
-import Wallet from './Wallet';
+import { Wallet } from './Wallet';
 
 configure({adapter: new Adapter()});
 
