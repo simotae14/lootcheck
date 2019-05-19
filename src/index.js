61c0946e2b21de312f718b8f5b771b1a82c985fb
Simone Taeggi <startae14@gmail.com>
2019-05-19 17:24:53 +0200

    create the Redux Store

--- src/index.js
+++ src/index.js
@@ -1,6 +1,17 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 
+import rootReducer from './reducers/balance';
 import App from './components/App';
 
-render(<App />, document.getElementById('root'));
+// create the redux store
+const store = createStore(rootReducer);
+
+render(
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    document.getElementById('root')
+);
