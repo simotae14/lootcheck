71fbf95e37caf4501292d9cb01ab96bec96769df
Simone Taeggi <startae14@gmail.com>
2019-05-19 15:56:15 +0200

    first test, check if the browser renders correctly App

--- src/components/App.test.js
+++ src/components/App.test.js
@@ -7,5 +7,10 @@ import App from './App';
 configure({adapter: new Adapter()});
 
 describe('App', () => {
-
+    // create the shallow rendering
+    const app = shallow(<App />);
+    // 1st unit test, renders correctly App
+    it('renders correctly', () => {
+        expect(toJson(app)).toMatchSnapshot();
+    });
 });
