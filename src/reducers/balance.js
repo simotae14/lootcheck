113c4bc76b7c5343e2840feb465f9bc465cffc0c
Simone Taeggi <startae14@gmail.com>
2019-05-19 20:55:09 +0200

    add the code in the reducer to pass the deposit test

--- src/reducers/balance.js
+++ src/reducers/balance.js
@@ -4,6 +4,8 @@ const balance = (state = 0, action) => {
     switch(action.type) {
         case constants.SET_BALANCE:
             return action.balance;
+        case constants.DEPOSIT:
+            return state + action.deposit;
         default:
             return state;
     }
