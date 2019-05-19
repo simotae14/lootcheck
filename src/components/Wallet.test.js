0c90bb624719528a883071fa36c9cba141c31dc2
Simone Taeggi <startae14@gmail.com>
2019-05-19 22:28:11 +0200

    test if when I insert a value inside the input wallet it updates the local state of the Wallet component

--- src/components/Wallet.test.js
+++ src/components/Wallet.test.js
@@ -25,4 +25,17 @@ describe('Wallet', () => {
     it('creates an input to deposit into or withdraw from the balance', () => {
         expect(wallet.find('.input-wallet').exists()).toBe(true);
     });
+
+    describe('when the user types into the wallet input', () => {
+        const userBalance = '25';
+
+        beforeEach(() => {
+            wallet.find('.input-wallet')
+                .simulate('change', { target: { value: userBalance } });
+        });
+
+        it('updates the local wallet balance in the `state` and it converts it to a number', () => {
+            expect(wallet.state().balance).toEqual(parseInt(userBalance, 10));
+        });
+    });
 });
