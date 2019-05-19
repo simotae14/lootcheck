03336b4b6baa6d9829f32968a6941c5b0f94ec26
Simone Taeggi <startae14@gmail.com>
2019-05-19 16:48:05 +0200

    add a test to check the actionCreator setBalance

---
+++ src/actions/balance.test.js
@@ -0,0 +1,14 @@
+import * as constants from './constants';
+import * as actions from './balance';
+
+// check if an action creator create an action to set the balance
+it('creates an action to set the balance', () => {
+    const balance = 0;
+    const expectedAction = {
+        type: constants.SET_BALANCE,
+        balance
+    };
+
+    // check if the action creator return the expectedAction
+    expect(action.setBalance(balance)).toEqual(expectedAction);
+});
