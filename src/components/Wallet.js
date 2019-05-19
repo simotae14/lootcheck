4e13113de970de5c93c52a362d7e53bb3d8c27ba
Simone Taeggi <startae14@gmail.com>
2019-05-19 22:35:12 +0200

    create the local state and the change method to pass the test

--- src/components/Wallet.js
+++ src/components/Wallet.js
@@ -2,6 +2,14 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
 export class Wallet extends Component {
+    constructor() {
+        super();
+        this.state = {
+            balance: undefined
+        };
+    }
+    updateBalance = e => this.setState({ balance: parseInt(e.target.value, 10)});
+
     render() {
         return (
             <div>
@@ -9,7 +17,10 @@ export class Wallet extends Component {
                     Wallet Balance: {this.props.balance}
                 </h3>
                 <br />
-                <input className="input-wallet"/>
+                <input
+                    onChange={this.updateBalance}
+                    className="input-wallet"
+                />
             </div>
         );
     }
