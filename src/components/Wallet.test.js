8cf2ce1f781ed7dc221abef8883c79764d309d63
Simone Taeggi <startae14@gmail.com>
2019-05-19 18:55:30 +0200

    test if the Wallet show the correct balance

--- src/components/Wallet.test.js
+++ src/components/Wallet.test.js
@@ -7,10 +7,18 @@ import { Wallet } from './Wallet';
 configure({adapter: new Adapter()});
 
 describe('Wallet', () => {
+    // props
+    const props = {
+        balance: 20
+    };
     // create the shallow version of Wallet
-    const wallet = shallow(<Wallet />);
+    const wallet = shallow(<Wallet {...props} />);
 
     it('renders properly', () => {
         expect(wallet).toMatchSnapshot();
+    });
+
+    it('displays the balance from props', () => {
+        expect(wallet.find('.balance').text()).toEqual('Wallet balance: 20');
     });
 });
