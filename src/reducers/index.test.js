56fc8cfd97c24920c4c9b42994b2f125802ad6cd
Simone Taeggi <startae14@gmail.com>
2019-05-20 10:26:45 +0200

    test combine reducers

---
+++ src/reducers/index.test.js
@@ -0,0 +1,10 @@
+import rootReducer from './index';
+
+describe('rootReducer', () => {
+  it('initializes the default state', () => {
+    expect(rootReducer({}, {})).toEqual({
+        balance: 0,
+        bitcoin: {}
+    });
+  });
+});
