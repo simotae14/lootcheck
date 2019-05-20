e936bd9f8601a7c79a5d909d59ad49bc942c0bd2
Simone Taeggi <startae14@gmail.com>
2019-05-20 11:54:14 +0200

    test displays the correct bitcoin data

--- src/components/Loot.test.js
+++ src/components/Loot.test.js
@@ -25,6 +25,27 @@ describe('Loot', () => {
         it('dispatches the `fetchBitcoin()` method it receives from props', () => {
             expect(mockFetchbitcoin).toHaveBeenCalled();
         });
+
+        describe('when there are valid bitcoin props', () => {
+            beforeEach(() => {
+                props =  {
+                    balance: 10,
+                    bitcoin: {
+                        bpi: {
+                            USD: {
+                                rate: '1,000'
+                            }
+                        }
+                    }
+                };
+                loot = shallow(<Loot {...props} />);
+            });
+
+            it('displays the correct bitcoin value', () => {
+                expect(loot.find('h3').text()).toEqual('Bitcoin balance: 0.01');
+            });
+        });
+
     });
 
 });
