cdb290b4656e1d21ab36a04d50d57a033391d29f
Simone Taeggi <startae14@gmail.com>
2019-05-20 11:15:19 +0200

    test if on mount of the Loot component it dispatch the fetchBitcoin action

--- src/components/Loot.test.js
+++ src/components/Loot.test.js
@@ -1,15 +1,34 @@
 import React from 'react';
 import Adapter from 'enzyme-adapter-react-16';
-import { shallow, configure } from 'enzyme';
+import { shallow, configure, mount } from 'enzyme';
 import toJson from 'enzyme-to-json';
 import { Loot } from './Loot';
 
 configure({adapter: new Adapter()});
 
 describe('Loot', () => {
-    const loot = shallow(<Loot />);
+    // mock fetch
+    const mockFetchBitcoin = jest.fn();
+    // props
+    const props = {
+        balance: 10,
+        bitcoin: {}
+    };
+    let loot = shallow(<Loot {...props} />);
 
     it('renders properly', () => {
         expect(toJson(loot)).toMatchSnapshot();
     });
+
+    describe('when mounted', () => {
+        beforeEach(() => {
+            props.fetchBitcoin = mockFetchBitcoin;
+            loot = mount(<Loot {...props} />);
+        });
+
+        it('dispatches the `fetchBitcoin()` method it receives from props', () => {
+            expect(mockFetchBitcoin).toHaveBeenCalled();
+        });
+    });
+
 });
